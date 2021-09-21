1878da3dcf8be691d01b633cad6c8e0afd6a932b
fake-empire <74241701+fake-empire@users.noreply.github.com>
2021-09-21 11:04:47 +1000

    Integrated API with forecast JS

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thur", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col-2">
-              <div class="weather-forecast-day">${day}</div>
-           <img src="http://openweathermap.org/img/wn/03d@2x.png" id="icon-one" width="36px">
-           <div class="weather-forecast-temp"><span class="weather-forecast-temp-max">18°</span> <span class="weather-forecast-temp-min">12°</span></div>
+              <div class="weather-forecast-day">${formatDay(
+                forecastDay.dt
+              )}</div>
+           <img src="http://openweathermap.org/img/wn/${
+             forecastDay.weather[0].icon
+           }@2x.png" id="icon-one" width="36px">
+           <div class="weather-forecast-temp"><span class="weather-forecast-temp-max">${Math.round(
+             forecastDay.temp.max
+           )}°</span> <span class="weather-forecast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span></div>
             </div>
             `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
 
@@ -77,8 +95,6 @@ function displayCelsiusTemp(event) {
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
-displayForecast();
 
 let celsiusTemperature = null;
 
